6b7f85fc937b13fd1e00b39fe952001972666cf7
Arjun GS <74770032+arjungsanal@users.noreply.github.com>
2021-06-03 17:01:36 +0530

    Update constants.jsx

--- src/Constants/constants.jsx
+++ src/Constants/constants.jsx
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
-export const API_KEY = '5cd932719e68989a5bd3f96ae9614834';
+export const API_KEY = 'API-KEY HERE';
 export const imgBaseUrl = 'https://image.tmdb.org/t/p/original';
